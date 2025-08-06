11f2e4b5d725a320883d6bab797be2bcc9a02b2e
dinis <denismarkes2006@gmail.com>
2025-08-06 16:38:44 +0000

    Refactor error handling in error_embed and update guild synchronization in main.py

--- embeds/error_embed.py
+++ embeds/error_embed.py
@@ -1,17 +1,29 @@
 import discord
+import json
 
 def error_embed(error_code, error_message):
     print(f"[DEBUG] Creating error embed for code: {error_code}")
 
-    if error_code == 429:
-        title = "SLOW THE FUCK DOWN!"
-        description = "Error 429: Too Many Requests"
-    elif error_code == 404:
-        title = "I'M LOST"
-        description = "Error 404: Not Found"
-    else:
-        title = "I AM A BAD BOT AND I HAVE FAILED!"
-        description = f"Error {error_code}: {error_message}"
+    try:
+        data = json.loads(error_message)
+        real_message = data["errors"][0]["message"]
+    except Exception:
+        real_message = error_message
+
+    match error_code:
+        case 400:
+            title = "WHOEVER DID THIS PROGRAM CAN'T CODE FOR SHIT!"
+        case 403:
+            title = "LISTEN HERE, IT'S NOT ME, IT'S ANILIST FAULT!"
+        case 404:
+            title = "I'M LOST! :)"
+        case 429:
+            title = "SLOW THE FUCK DOWN!"
+        case _:
+            title = "I AM A BAD BOT AND I HAVE FAILED!"
+    
+
+    description = f"Error {error_code}: {real_message}"
    
     embed = discord.Embed(
             title=title,
--- main.py
+++ main.py
@@ -8,42 +8,63 @@ from commands import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-SERVER_ID = os.getenv('GUILD_ID')
+GUILD_IDS = os.getenv('GUILD_IDS').split(',')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-guild = discord.Object(id=SERVER_ID) 
+guilds = [discord.Object(id=int(gid.strip())) for gid in GUILD_IDS]
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
-    await bot.tree.sync(guild=guild)
+    for guild in guilds:
+        await bot.tree.sync(guild=guild)
 
-@bot.tree.command(name="add-user", description="Adds user to registered users in bot", guild=guild)
+@bot.tree.command(
+    name="add-user",
+    description="Adds user to registered users in bot",
+    guilds=guilds
+)
 @app_commands.describe(username="Anilist username")
 async def add_user(interaction: discord.Interaction, username: str):
     await add_user_command(interaction, username)
 
 
-@bot.tree.command(name="affinity", description="Calculates the affinity between the user and all the other users in the guild", guild=guild)
+@bot.tree.command(
+    name="affinity",
+    description="Calculates the affinity between the user and all the other users in the guild",
+    guilds=guilds
+)
 @app_commands.describe()
 async def affinity(interaction: discord.Interaction):
     await affinity_command(interaction)
 
 
-@bot.tree.command(name="anime", description="Get info about an anime", guild=guild)
+@bot.tree.command(
+    name="anime",
+    description="Get info about an anime",
+    guilds=guilds
+)
 @app_commands.describe(search="Anime title or id to search")
 async def anime(interaction: discord.Interaction, search: str):
     await media_command(interaction, search, "ANIME")
 
-@bot.tree.command(name="compare-users", description="Calculates the affinity between two anilist users", guild=guild)
+@bot.tree.command(
+    name="compare-users",
+    description="Calculates the affinity between two anilist users",
+    guilds=guilds
+)
 @app_commands.describe(username1="Anilist username", username2="Anilist username")
 async def compare_users(interaction: discord.Interaction, username1: str, username2: str):
     await compare_users_command(interaction, username1, username2)
 
-@bot.tree.command(name="manga", description="Get info about a manga", guild=guild)
+@bot.tree.command(
+    name="manga",
+    description="Get info about a manga",
+    guilds=guilds
+)
 @app_commands.describe(search="Manga title or id to search")
 async def manga(interaction: discord.Interaction, search: str):
     await media_command(interaction, search, "MANGA")
